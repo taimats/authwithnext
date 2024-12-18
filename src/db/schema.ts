3608a89f63868a3e0a2e2d45c9b872f065aa09fe
taimats <beo8939@gmail.com>
2024-12-19 00:03:58 +0900

    drizzleのセットアップ

---
+++ src/db/schema.ts
@@ -0,0 +1,5 @@
+import { text, pgTable } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: text("id").primaryKey(),
+});
--- src/env.ts
+++ src/env.ts
@@ -4,9 +4,11 @@ import { z } from "zod";
 export const env = createEnv({
     server: {
         NODE_ENV: z.enum(["development", "production"]),
+        DATABASE_URL: z.string().url(),
     },
     client: {},
     runtimeEnv: {
         NODE_ENV: process.env.NODE_ENV,
+        DATABASE_URL: process.env.DATABASE_URL,
     },
 });
