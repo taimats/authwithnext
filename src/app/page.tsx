b46662db7186813a88b56fd6aee843e5450dfc7c
taimats <beo8939@gmail.com>
2024-12-18 21:24:26 +0900

    環境変数の設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { env } from "@/env";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <p className="text-green-200 font-bold text-8xl">Hello World</p>
+      <p className="text-green-400 font-bold text-8xl">Hello World</p>
+      {env.NODE_ENV}
     </main>
   )
 }
